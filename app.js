8c164258ed8f9593b5ba56c5440ef021bf6236ca
Olga Bernal <obersan6@gmail.com>
2024-01-26 13:22:08 -0500

    changes to app and style

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Wait for the DOM to be fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', function() {
-         // Array containing URLs of card images
         const cards = [
             'images/1.jpg',
             'images/2.jpg',
@@ -23,27 +22,21 @@ document.addEventListener('DOMContentLoaded', function() {
             'images/9.jpg',
             'images/10.jpg'
         ];
-     // Select all card elements from the HTML    
+    
     const divs = document.querySelectorAll('.card');
     
-    // Counter var for the number of clicks, and an array to store clicked cards
     let numberOfClicks = 0;
     let clickedCards = [];
 
-    // Shuffle the card image URLs at the beginning of the game using the function "shuffleCards()"
     shuffleCards(cards); 
 
-    // Loop through all card html elements (all the divs)
     for (let i = 0; i < divs.length; i++) {
         const div = divs[i]; 
         const img = div.querySelector('img'); 
         const cardValue = cards[i]; 
 
-        // Apply styling to the card image element
         img.classList.add('img-card'); 
-        // Set the source attribute of the card image to an empty string initially
         img.setAttribute('src', ''); 
-        // Set the data attribute of the card element to the corresponding card image URL
         div.setAttribute('data-card', cardValue); 
 
         // Add a 'click' event listener to each card element (the div)
@@ -56,13 +49,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 numberOfClicks++;
             }
 
-            // Check if two cards have been clicked
             if (numberOfClicks === 2) {
-                // Set a timer of 1 second every two clicks
                 setTimeout(function() {
-                    // Check if the clicked cards are a match
-                    if (clickedCards[0].cardValue === clickedCards[1].cardValue) {
-                    } else { 
+                    if (clickedCards[0].cardValue !== clickedCards[1].cardValue) {
                         // Handle the case when the cards are not a match
                         clickedCards.forEach(({ div, cardValue }) => { 
                             div.classList.remove('flipped'); 
@@ -79,7 +68,6 @@ document.addEventListener('DOMContentLoaded', function() {
     
     //Declare a function to shuffle the array "cards" (which contains the URLs of card images)
     function shuffleCards (array) {
-        // Loop over the array starting from the last element
         for (let i = array.length - 1; i > 0; i--) {
             // Generate a random index (img from 'cards') and swap elements
             const j = Math.floor(Math.random() * (i + 1)); 
